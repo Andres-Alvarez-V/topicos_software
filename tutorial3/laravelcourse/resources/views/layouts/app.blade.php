b87a8b2f98bbf531796944e28e013dd21b853856
Camilo Alvarez <camilo.alvarez@pragma.com.co>
2023-08-27 19:09:30 -0500

    v3.0 - Dependencias, auth, sesion

--- tutorial3/laravelcourse/resources/views/layouts/app.blade.php
+++ tutorial3/laravelcourse/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
 
     <meta charset="utf-8" />
@@ -12,6 +13,7 @@
 
     <title>@yield('title', 'Online Store')</title>
 </head>
+
 <body>
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
@@ -24,6 +26,16 @@
                 <div class="navbar-nav ms-auto">
                     <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
                     <a class="nav-link active" href="{{ route('home.about') }}">About</a>
+                    <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                    @guest
+                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                    @else
+                    <form id="logout" action="{{ route('logout') }}" method="POST">
+                        <a role="button" class="nav-link active" onclick="document.getElementById('logout').submit();">Logout</a>
+                        @csrf
+                    </form>
+                    @endguest
                 </div>
             </div>
         </div>
